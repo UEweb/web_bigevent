a7a3df1517ea77d0a23401a811b6f7674be1fd5f
youyi <loli1320@163.com>
2023-02-25 10:30:15 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,25 @@
 //会先调用ajaxPrefilter()这个函数
 //在ajaxPrefilter这个函数中可以拿到我们给ajax的配置对象
 $.ajaxPrefilter(function (options) {
+    //在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url)
+
+    //统一为有权限的接口设置headers请求头
+   if(options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+        Authorization: localStorage.getItem('token') || ''
+    }
+   }
+
+   //全局统一挂载complete 回调函数
+   options.complete = function (res) {
+     //在complete回调函数中，可以使用res.responseJSON拿到服务器相应回来的数据
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+             //1.清空token
+             localStorage.removeItem('token')
+              //2.跳转到登录页面
+             location.href = '/login.html'
+          }
+   }
+
 })
